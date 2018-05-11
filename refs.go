98e9cfe4d6ff8a0d41096fae38ba86955ae4bfe7
keks <keks@cryptoscope.co>
2018-05-11 15:55:29 +0200

    moar refs

--- refs.go
+++ refs.go
@@ -3,7 +3,74 @@ package sbot
 import (
 	"encoding/base64"
 	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
 )
+
+const (
+	RefAlgoSHA256  = "sha256"
+	RefAlgoEd25519 = "ed25519"
+)
+
+var (
+	ErrInvalidRef     = errors.New("Invalid Ref")
+	ErrInvalidRefType = errors.New("Invalid Ref Type")
+	ErrInvalidRefAlgo = errors.New("Invalid Ref Algo")
+	ErrInvalidSig     = errors.New("Invalid Signature")
+	ErrInvalidHash    = errors.New("Invalid Hash")
+)
+
+func ParseRef(str string) (Ref, error) {
+	if len(str) == 0 {
+		return nil, ErrInvalidRef
+	}
+
+	split := strings.Split(str[1:], ".")
+	if len(split) != 2 {
+		return nil, ErrInvalidRef
+	}
+
+	raw, err := base64.StdEncoding.DecodeString(split[0])
+	if err != nil {
+		raw, err = base64.URLEncoding.DecodeString(split[1])
+		if err != nil {
+			return nil, ErrInvalidHash
+		}
+	}
+
+	switch str[0:1] {
+	case "@":
+		if split[1] != "ed25519" {
+			return nil, ErrInvalidRefAlgo
+		}
+
+		return &FeedRef{
+			ID:   raw,
+			Algo: RefAlgoEd25519,
+		}, nil
+	case "%":
+		if split[1] != "sha256" {
+			return nil, ErrInvalidRefAlgo
+		}
+
+		return &MessageRef{
+			Hash: raw,
+			Algo: RefAlgoEd25519,
+		}, nil
+	case "&":
+		if split[1] != "sha256" {
+			return nil, ErrInvalidRefAlgo
+		}
+
+		return &BlobRef{
+			Hash: raw,
+			Algo: RefAlgoEd25519,
+		}, nil
+	}
+
+	return nil, ErrInvalidRefType
+}
 
 type Ref interface {
 	Ref() string
