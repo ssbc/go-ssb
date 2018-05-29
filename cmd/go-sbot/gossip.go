c99084b6de11876a30fd872a37e4019ec1ed804e
Henry <cryptix@riseup.net>
2018-05-29 18:23:37 +0200

    split connect from HandleCall

--- cmd/go-sbot/gossip.go
+++ cmd/go-sbot/gossip.go
@@ -36,54 +36,52 @@ func (c *gossip) HandleCall(ctx context.Context, req *muxrpc.Request) {
 
 	switch req.Method.String() {
 	case "gossip.connect":
+		if len(req.Args) != 1 {
+			// TODO: use secretstream
+			log.Log("error", "usage", "args", req.Args, "method", req.Method)
+			checkAndClose(errors.New("usage: gossip.connect host:port:key"))
+			return
+		}
+
+		destString, ok := req.Args[0].(string)
+		if !ok {
+			err := errors.Errorf("gossip.connect call: expected argument to be string, got %T", req.Args[0])
+			checkAndClose(err)
+			return
+		}
+
+		if err := c.connect(ctx, destString); err != nil {
+			err := errors.Wrap(err, "gossip.connect failed.")
+			checkAndClose(err)
+			return
+		}
 	default:
 		checkAndClose(errors.Errorf("unknown command: %s", req.Method))
 	}
+}
 
-	if len(req.Args) != 1 {
-		// TODO: use secretstream
-		log.Log("error", "usage", "args", req.Args, "method", req.Method)
-		checkAndClose(errors.New("usage: gossip.connect host:port:key"))
-		return
-	}
-
-	destString, ok := req.Args[0].(string)
-	if !ok {
-		err := errors.Errorf("gossip.connect call: expected argument to be string, got %T\n", req.Args[0])
-		checkAndClose(err)
-		return
-	}
-
-	splitted := strings.Split(destString, ":")
+func (g *gossip) connect(ctx context.Context, dest string) error {
+	splitted := strings.Split(dest, ":")
 	if n := len(splitted); n != 3 {
-		checkAndClose(errors.Errorf("gossip.connect: bad request. expected 3 parts, got %d", n))
-		return
+		return errors.Errorf("gossip.connect: bad request. expected 3 parts, got %d", n)
 	}
 
 	addr, err := net.ResolveTCPAddr("tcp", strings.Join(splitted[:2], ":"))
 	if err != nil {
-		err = errors.Wrapf(err, "gossip.connect call: error resolving network address %q", splitted[:2])
-		checkAndClose(err)
-		return
+		return errors.Wrapf(err, "gossip.connect call: error resolving network address %q", splitted[:2])
 	}
 
 	ref, err := sbot.ParseRef(splitted[2])
 	if err != nil {
-		err = errors.Wrapf(err, "gossip.connect call: failed to parse FeedRef %s", splitted[2])
-		checkAndClose(err)
-		return
+		return errors.Wrapf(err, "gossip.connect call: failed to parse FeedRef %s", splitted[2])
 	}
 
 	remoteFeed, ok := ref.(*sbot.FeedRef)
 	if !ok {
-		checkAndClose(errors.Errorf("gossip.connect: expected FeedRef got %T", ref))
-		return
+		return errors.Errorf("gossip.connect: expected FeedRef got %T", ref)
 	}
 
 	wrappedAddr := netwrap.WrapAddr(addr, secretstream.Addr{PubKey: remoteFeed.ID})
-	err = c.Node.Connect(ctx, wrappedAddr)
-	if err != nil {
-		err = errors.Wrapf(err, "gossip.connect call: error connecting to %q", addr)
-		checkAndClose(err)
-	}
+	err = g.Node.Connect(ctx, wrappedAddr)
+	return errors.Wrapf(err, "gossip.connect call: error connecting to %q", addr)
 }
