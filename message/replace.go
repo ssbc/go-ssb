5e9b5ce23a40ddc54b086bd9eaca92f598c6f581
Henry <cryptix@riseup.net>
2018-10-07 13:07:14 +0200

    fix encode unicodeEscapeSome
    this was supposed to be in hex all along...

--- message/replace.go
+++ message/replace.go
@@ -16,7 +16,7 @@ var signatureRegexp = regexp.MustCompile(",\n  \"signature\": \"([A-Za-z0-9/+=.]
 func unicodeEscapeSome(s string) string {
 	var b bytes.Buffer
 	for i, r := range s {
-		if r < 20 {
+		if r < 0x20 {
 			// TODO: width for multibyte chars
 			runeValue, _ := utf8.DecodeRuneInString(s[i:])
 			fmt.Fprintf(&b, "\\u%04x", runeValue)
