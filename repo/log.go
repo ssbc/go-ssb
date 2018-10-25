2c6e8874077a63ba3397a58e170fd4e001831f44
keks <keks@cryptoscope.co>
2018-10-25 11:05:26 +0200

    offset2 log
    new format is not a single file but two! data and offset
    this allows for gapless files with way less overhead
    
    * gossip replicate test: add testdata for offset2 log
    * repo: use offset2 log
    * repo: use r.GetPath and update testdata

--- repo/log.go
+++ repo/log.go
@@ -1,14 +1,11 @@
 package repo
 
 import (
-	"os"
-
 	"github.com/pkg/errors"
 
 	"go.cryptoscope.co/margaret"
 	"go.cryptoscope.co/margaret/codec/msgpack"
-	"go.cryptoscope.co/margaret/framing/lengthprefixed"
-	"go.cryptoscope.co/margaret/offset"
+	"go.cryptoscope.co/margaret/offset2"
 
 	"go.cryptoscope.co/ssb/message"
 )
@@ -20,13 +17,7 @@ func OpenLog(r Interface, path ...string) (margaret.Log, error) {
 		path[0] = "logs"
 	}
 
-	logFile, err := os.OpenFile(r.GetPath(path...), os.O_CREATE|os.O_RDWR, 0600)
-	if err != nil {
-		return nil, errors.Wrap(err, "error opening log file")
-	}
-
 	// TODO use proper log message type here
-	// FIXME: 16kB because some messages are even larger than 12kB - even though the limit is supposed to be 8kb
-	log, err := offset.New(logFile, lengthprefixed.New32(16*1024), msgpack.New(&message.StoredMessage{}))
+	log, err := offset2.Open(r.GetPath(path...), msgpack.New(&message.StoredMessage{}))
 	return log, errors.Wrap(err, "failed to open log")
 }
