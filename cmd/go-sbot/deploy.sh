a64b33e2d89fc2be3d877d5de3e3691415e8c241
Henry <cryptix@riseup.net>
2018-12-29 13:55:33 +0100

    sbot deploy script

---
+++ cmd/go-sbot/deploy.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -e 
+export CGO_ENABLED=0
+export GOROOT=$HOME/go.tmproot
+
+export GOOS=linux
+go install -v
+go build -v -i
+gzip go-sbot
+scp go-sbot.gz keksvps:.
+rm go-sbot.gz
+
+export GOOS=freebsd
+go build -v -i
+gzip go-sbot
+scp go-sbot.gz aufdie12:.
+# todo: restart
+scp go-sbot.gz vmbox:.
+# todo: restart
+rm go-sbot.gz
