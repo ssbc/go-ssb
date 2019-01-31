3f2aa98aab3fb70c4d90068168c185690da91163
Henry <cryptix@riseup.net>
2019-01-31 13:43:07 +0100

    messages: add KeyValue wrapping type
    this is how jsbot returnes the key for a message using --keys on
    createHistoryStream and createLogStream

--- message/requests.go
+++ message/requests.go
@@ -62,3 +62,9 @@ type SignedLegacyMessage struct {
 	LegacyMessage
 	Signature Signature `json:"signature"`
 }
+
+type KeyValueRaw struct {
+	Key       *ssb.MessageRef `json:"key"`
+	Value     json.RawMessage `json:"value"`
+	Timestamp int64           `json:"timestamp"`
+}
