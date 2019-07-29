39efa9d991aa27d483f38077307dc48b6c43fc82
John <john@server.ky>
2019-07-29 15:47:42 +0200

    indirect: elaborate on error

--- internal/mutil/indirect.go
+++ internal/mutil/indirect.go
@@ -43,7 +43,11 @@ func (il indirectLog) Query(args ...margaret.QuerySpec) (luigi.Source, error) {
 	}
 
 	return mfr.SourceMap(src, func(ctx context.Context, v interface{}) (interface{}, error) {
-		return il.root.Get(v.(margaret.Seq))
+		vSeq, ok := v.(margaret.Seq)
+		if !ok {
+			return nil, errors.New("indirect requires values to be pair with their sequence")
+		}
+		return il.root.Get(vSeq)
 	}), nil
 }
 
