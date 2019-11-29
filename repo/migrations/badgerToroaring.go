fba6b6d604b4231be46d461b6c82f465a8a27db0
Henry <cryptix@riseup.net>
2019-11-29 15:56:06 +0100

    migrations: add func to find repos that still use badger

---
+++ repo/migrations/badgerToroaring.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/cryptix/go/logging"
+	"github.com/go-kit/kit/log/level"
+	"github.com/pkg/errors"
+	"go.cryptoscope.co/ssb/repo"
+	"os"
+)
+
+func StillUsingBadger(log logging.Interface, r repo.Interface) (bool, error) {
+	v := CurrentVersion(r)
+	switch {
+	case v == 1:
+		// do the deed
+	case v < 1:
+		level.Error(log).Log("event", "repo is not version 1 yet", "v", v)
+		return false, nil
+	default:
+		return false, errors.Errorf("sbot/repo migrate: invalid version: %d", v)
+	}
+
+	// check the db and the state file
+	var hasDB, hasState bool
+
+	dbPath := r.GetPath(repo.PrefixMultiLog, "userFeeds", "db")
+	_, err := os.Stat(dbPath)
+	if err != nil && !os.IsNotExist(err) {
+		return false, errors.Wrap(err, "StillUsingBadger: failed to check old db path inside the repo")
+	}
+	if err == nil {
+		hasDB = true
+	}
+
+	stateFilePath := r.GetPath(repo.PrefixMultiLog, "userFeeds", "state.json")
+	_, err = os.Stat(stateFilePath)
+	if err != nil && !os.IsNotExist(err) {
+		return false, errors.Wrap(err, "StillUsingBadger: failed to check old state file path inside the repo")
+	}
+	if err == nil {
+		hasState = true
+	}
+
+	return hasDB && hasState, nil
+}
