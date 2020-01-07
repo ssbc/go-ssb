d88eb706b903d6f9c07362f8b20c93c39f316b03
Henry <cryptix@riseup.net>
2020-01-07 12:26:18 +0100

    overhaul FSCK error return

--- sbot/status.go
+++ sbot/status.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/dustin/go-humanize"
-	"github.com/go-kit/kit/log/level"
 	"github.com/pkg/errors"
 	"go.cryptoscope.co/margaret"
 	"go.cryptoscope.co/margaret/multilog"
@@ -64,64 +63,68 @@ func (bct byConnTime) Swap(i int, j int) {
 	bct[i], bct[j] = bct[j], bct[i]
 }
 
-func (s *Sbot) FSCK(idxlog multilog.MultiLog) {
-
-	checkFatal := func(err error) {
-		if err != nil {
-			level.Error(s.info).Log("error", "fsck failed", "err", err)
-			// s.Shutdown()
-			// s.Close()
-			// os.Exit(1)
-
-			return
-		}
-	}
-
+func (s *Sbot) FSCK(idxlog multilog.MultiLog) error {
 	if idxlog == nil {
 		var ok bool
 		idxlog, ok = s.GetMultiLog(multilogs.IndexNameFeeds)
 		if !ok {
-			checkFatal(errors.Errorf("no multilog"))
-			return
+			return errors.Errorf("sbot: no users multilog")
 		}
 	}
 
-	// gb := s.GraphBuilder
-
 	feeds, err := idxlog.List()
-	checkFatal(err)
+	if err != nil {
+		return err
+	}
 
-	// var followCnt, msgCount uint
 	for _, author := range feeds {
 		var sr ssb.StorageRef
 		err := sr.Unmarshal([]byte(author))
-		checkFatal(err)
+		if err != nil {
+			return err
+		}
 		authorRef, err := sr.FeedRef()
-		checkFatal(err)
+		if err != nil {
+			return err
+		}
 
 		subLog, err := idxlog.Get(author)
-		checkFatal(err)
-
-		userLogV, err := subLog.Seq().Value()
-		checkFatal(err)
-		userLogSeq := userLogV.(margaret.Seq)
-		rlSeq, err := subLog.Get(userLogSeq)
-		if margaret.IsErrNulled(err) {
-			continue
-		} else {
-			checkFatal(err)
+		if err != nil {
+			return err
 		}
+
+		currentSeqV, err := subLog.Seq().Value()
+		if err != nil {
+			return err
+		}
+		currentSeqFromIndex := currentSeqV.(margaret.Seq)
+		rlSeq, err := subLog.Get(currentSeqFromIndex)
+		if err != nil {
+			if margaret.IsErrNulled(err) {
+				continue
+			}
+			return err
+		}
+
 		rv, err := s.RootLog.Get(rlSeq.(margaret.BaseSeq))
-		if margaret.IsErrNulled(err) {
-			continue
-		} else {
-			checkFatal(err)
+		if err != nil {
+			if margaret.IsErrNulled(err) {
+				continue
+			}
+			return err
 		}
 		msg := rv.(ssb.Message)
 
-		if msg.Seq() != userLogSeq.Seq()+1 {
-			err = fmt.Errorf("light fsck failed: head of feed mismatch on %s: %d vs %d", authorRef.Ref(), msg.Seq(), userLogSeq.Seq()+1)
-			checkFatal(err)
+		if msg.Seq() != currentSeqFromIndex.Seq()+1 {
+			err = fmt.Errorf("light fsck failed: head of feed mismatch on %s: %d vs %d",
+				authorRef.Ref(),
+				msg.Seq(),
+				currentSeqFromIndex.Seq()+1)
+			if err != nil {
+				return err
+			}
 		}
 	}
+
+	return nil
 }
