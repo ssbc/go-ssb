6b3103a11715fb6994ccec3be77fcbdc3b4840d3
Henry <cryptix@riseup.net>
2020-01-15 11:49:49 +0100

    some inspector tooling

---
+++ cmd/ssb-logcat/main.go
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime/debug"
+	"strconv"
+
+	"go.cryptoscope.co/margaret"
+	"go.cryptoscope.co/ssb"
+	"go.cryptoscope.co/ssb/repo"
+
+	"github.com/pkg/errors"
+	"go.cryptoscope.co/luigi"
+)
+
+func check(err error) {
+	if err != nil {
+		fail(err)
+	}
+}
+
+func fail(err error) {
+	fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	fmt.Fprintln(os.Stderr, "occurred at")
+	debug.PrintStack()
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Fprintln(os.Stderr, "usage: ssb-logcat <repo> <startSeq> <limit>")
+		os.Exit(1)
+	}
+	fromPath := os.Args[1]
+
+	startSeqInt, err := strconv.Atoi(os.Args[2])
+	check(err)
+	startSeq := margaret.BaseSeq(startSeqInt)
+
+	limit, err := strconv.Atoi(os.Args[3])
+	check(err)
+
+	repoFrom := repo.New(fromPath)
+
+	from, err := repo.OpenLog(repoFrom)
+	check(err)
+
+	// fmt.Println("element count in source log:", seq)
+	// start := time.Now()
+	src, err := from.Query(margaret.Gt(startSeq), margaret.Limit(limit), margaret.SeqWrap(true))
+	check(err)
+	err = src.(luigi.PushSource).Push(context.TODO(), luigi.FuncSink(func(ctx context.Context, v interface{}, err error) error {
+		if err == (luigi.EOS{}) {
+			return nil
+		}
+		if err != nil {
+			return errors.Wrap(err, "push failed")
+		}
+
+		sw := v.(margaret.SeqWrapper)
+
+		sv := sw.Value()
+
+		msg, ok := sv.(ssb.Message)
+		if !ok {
+			panic("wrong message type")
+		}
+		os.Stdout.WriteString(fmt.Sprintf(`
+		{
+			"key": %q,
+			"rxSeq": %d,
+			"value":
+		`, msg.Key().Ref(), sw.Seq()))
+		os.Stdout.Write(msg.ValueContentJSON())
+		os.Stdout.WriteString("}\n")
+		return err
+	}))
+	check(err)
+
+}
---
+++ cmd/ssb-roaridx-inspector/main.go
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cryptix/go/logging"
+	"go.cryptoscope.co/margaret/multilog"
+	mlroaring "go.cryptoscope.co/margaret/multilog/roaringfiles"
+	"go.cryptoscope.co/ssb"
+)
+
+var check = logging.CheckFatal
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <dir> (hasAddr)", os.Args[0])
+		os.Exit(1)
+	}
+	logging.SetupLogging(nil)
+	log := logging.Logger(os.Args[0])
+
+	dir := os.Args[1]
+
+	mlog, err := mlroaring.NewMKV(dir)
+	check(err)
+
+	/*
+		addrs, err := mlog.List()
+		check(errors.Wrap(err, "error listing multilog"))
+		log.Log("mlog", "opened", "list#", len(addrs))
+		for i, a := range addrs {
+			var sr ssb.StorageRef
+			err := sr.Unmarshal([]byte(a))
+			check(err)
+
+			sublog, err := mlog.Get(a)
+			check(err)
+			seqv, err := sublog.Seq().Value()
+			check(err)
+			log.Log("i", i, "addr", sr.Ref(), "seq", seqv)
+		}
+	*/
+
+	// check has
+	if len(os.Args) > 2 {
+		ref, err := ssb.ParseFeedRef(os.Args[2])
+		check(err)
+		has, err := multilog.Has(mlog, ref.StoredAddr())
+		log.Log("mlog", "has", "addr", ref.Ref(), "has?", has, "hasErr", err)
+
+		bmap, err := mlog.LoadBitmap(ref.StoredAddr())
+		check(err)
+		fmt.Println(bmap.GetCardinality())
+		fmt.Println(bmap.String())
+	}
+
+	check(mlog.Close())
+}
