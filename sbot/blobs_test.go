3a0ac470383c6e7627f03ab9c9d22f3cdda9e674
Henry <cryptix@riseup.net>
2020-02-11 18:56:33 +0100

    renable leakcheck for blobs

--- sbot/blobs_test.go
+++ sbot/blobs_test.go
@@ -21,13 +21,14 @@ import (
 
 	"go.cryptoscope.co/ssb"
 	"go.cryptoscope.co/ssb/blobstore"
+	"go.cryptoscope.co/ssb/internal/leakcheck"
 	"go.cryptoscope.co/ssb/internal/testutils"
 )
 
 const blobSize = 1024 * 512
 
 func TestBlobsPair(t *testing.T) {
-	// defer leakcheck.Check(t)
+	defer leakcheck.Check(t)
 	r := require.New(t)
 	ctx, cancel := context.WithCancel(context.TODO())
 	botgroup, ctx := errgroup.WithContext(ctx)
@@ -352,7 +353,7 @@ func (s *session) eachOnBothWant(t *testing.T) {
 
 // check that we can get blobs from C to A through B
 func TestBlobsWithHops(t *testing.T) {
-	// // defer leakcheck.Check(t)
+	defer leakcheck.Check(t)
 	r := require.New(t)
 	a := assert.New(t)
 
@@ -492,7 +493,7 @@ func TestBlobsWithHops(t *testing.T) {
 // TODO: make extra test to make sure this doesn't turn into an echo chamber
 
 func TestBlobsTooBig(t *testing.T) {
-	// defer leakcheck.Check(t)
+	defer leakcheck.Check(t)
 	r := require.New(t)
 	ctx, cancel := context.WithCancel(context.TODO())
 
