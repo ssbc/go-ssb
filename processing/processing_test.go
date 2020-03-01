394bd4dc8290a0ed7e76ea2016672a9353e73ed9
keks <keks@cryptoscope.co>
2020-03-01 21:50:11 +0100

    processing: test more error cases

--- processing/processing_test.go
+++ processing/processing_test.go
@@ -3,6 +3,8 @@ package processing
 import (
 	"context"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,6 +18,83 @@ var enc vartuple.Encoding
 
 func init() {
 	enc.Binary = binary.BigEndian
+}
+
+func TestContentProcessorErrors(t *testing.T) {
+	var (
+		f = func(content map[string]interface{}) ([]string, error) {
+			switch content["trigger-error"] {
+			case "f":
+				return nil, errors.New("extractor error")
+			case "get":
+				return []string{"get-err"}, nil
+			case "append":
+				return []string{"append-err"}, nil
+			default:
+				t.Fatal("this should not happen!")
+				return nil, nil
+			}
+		}
+
+		msgFromContentString = func(content string) ssb.Message {
+			return &ssb.KeyValueRaw{
+				Value: ssb.Value{
+					Content: []byte(content),
+				},
+			}
+		}
+
+		// mkMockLog returns the mocked sublog for given addr
+		mkMockLog = func(addr librarian.Addr) margaret.Log {
+			return mockLog{
+				appendFunc: func(v interface{}) (margaret.Seq, error) {
+					var err error
+					if addr == "append-err" {
+						err = errors.New("append error")
+					}
+
+					return margaret.BaseSeq(0), err
+				},
+			}
+		}
+
+		// cp is the ContentProcessor that uses the mocked multilog
+		cp = ContentProcessor{
+			F: f,
+			MLog: mockMultilog{
+				getFunc: func(addr librarian.Addr) (margaret.Log, error) {
+					fmt.Sprintf("%x\n", addr)
+					if addr == "get-err" {
+						return nil, errors.New("get error")
+					}
+
+					return mkMockLog(addr), nil
+				},
+			},
+		}
+
+		ctx = context.TODO()
+
+		ins = []string{
+			`{"invalid": "json"`,
+			`{"trigger-error": "f"}`,
+			`{"trigger-error": "get"}`,
+			`{"trigger-error": "append"}`,
+		}
+
+		errs = []string{
+			`unexpected end of JSON input`,
+			`extractor error`,
+			`get error`,
+			`append error`,
+		}
+	)
+
+	for i, in := range ins {
+		seq := margaret.BaseSeq(i)
+		err := cp.ProcessMessage(ctx, msgFromContentString(in), seq)
+		require.EqualError(t, err, errs[i])
+	}
 }
 
 func TestContentProcessor(t *testing.T) {
@@ -114,12 +193,13 @@ func TestContentProcessor(t *testing.T) {
 				},
 				ins: []ssb.Message{
 					msgFromContentString(`{"foo": "bar"}`),
+					msgFromContentString(`"s0me/Ba5e64.box"`),
 					msgFromContentString(`{"bar": "baz"}`),
 					msgFromContentString(`{"foo": "bar", "bar": "baz"}`),
 				},
 				out: map[librarian.Addr][]margaret.Seq{
-					librarian.Addr(enc.Encode(nil, []byte("foo"), []byte("bar"))): seqsFromInts(0, 2),
-					librarian.Addr(enc.Encode(nil, []byte("bar"), []byte("baz"))): seqsFromInts(1, 2),
+					librarian.Addr(enc.Encode(nil, []byte("foo"), []byte("bar"))): seqsFromInts(0, 3),
+					librarian.Addr(enc.Encode(nil, []byte("bar"), []byte("baz"))): seqsFromInts(2, 3),
 				},
 			},
 		}
