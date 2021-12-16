5ba40ae36f988ba6457869f909ad7544cc3af9b9
cblgh <cblgh@cblgh.org>
2021-12-16 15:22:14 +0100

    set stricter default permissions for linux & other platforms

--- keys_default.go
+++ keys_default.go
@@ -11,4 +11,4 @@ import "os"
 
 // SecretPerms are the file permissions for holding SSB secrets.
 // We expect the file to only be accessable by the owner.
-var SecretPerms = os.FileMode(0600)
+var SecretPerms = os.FileMode(0400)
