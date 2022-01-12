41eb4b9e4fa1e3662555a12b0f36e826437f4e8d
cblgh <cblgh@cblgh.org>
2022-01-12 15:46:31 +0100

    start testing cli command

--- cmd/sbotcli/simple_test.go
+++ cmd/sbotcli/simple_test.go
@@ -5,6 +5,7 @@
 package main_test
 
 import (
+	"fmt"
 	"bytes"
 	"context"
 	"encoding/base64"
@@ -36,7 +37,7 @@ func buildCLI(t *testing.T) string {
 	return cliPath
 }
 
-// returns a func thay you can pass CLI arguments to and returns the output, which is mirroed to stderr for assertions.
+// returns a func which accepts CLI arguments, returns the output of running them. output is mirroed to stderr for assertions.
 func mkCommandRunner(t *testing.T, ctx context.Context, path string, sockPath string) func(...string) ([]byte, []byte) {
 	var stdout, stderr bytes.Buffer
 
@@ -92,6 +93,66 @@ func TestWhoami(t *testing.T) {
 
 	has := bytes.Contains(out, []byte(srv.KeyPair.ID().String()))
 	a.True(has, "ID not found in output")
+
+	srv.Shutdown()
+	err = srv.Close()
+	r.NoError(err)
+	r.NoError(<-errc)
+}
+
+func TestGetSubset(t *testing.T) {
+	cliPath := buildCLI(t)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	t.Cleanup(cancel)
+
+	r, a := require.New(t), assert.New(t)
+
+	srvRepo := filepath.Join("testrun", t.Name(), "serv")
+	os.RemoveAll(srvRepo)
+	srvLog := testutils.NewRelativeTimeLogger(os.Stderr)
+
+	srv, err := sbot.New(
+		sbot.WithInfo(srvLog),
+		sbot.WithRepoPath(srvRepo),
+		sbot.WithContext(ctx),
+		sbot.WithListenAddr(":0"),
+		sbot.LateOption(sbot.WithUNIXSocket()),
+	)
+	r.NoError(err, "sbot srv init failed")
+
+	var errc = make(chan error)
+	go func() {
+		errc <- srv.Network.Serve(ctx)
+	}()
+
+	a.EqualValues(-1, srv.ReceiveLog.Seq(), "log not empty")
+
+	sbotcli := mkCommandRunner(t, ctx, cliPath, filepath.Join(srvRepo, "socket"))
+
+	getPosts := []string{"subset", `{"op":"type", "string": "post"}`}
+	getFollows := []string{"subset", `{"op":"type", "string": "contact"}`}
+	getByAuthor := []string{"subset", fmt.Sprintf(`{"op":"author", "feed": "%s"}`, srv.KeyPair.ID())}
+	createPost := []string{"publish", "post", "post one: the first"}
+	// first we populate db slightly
+	_, _ = sbotcli(createPost...)
+	// then we query it
+	out, _ := sbotcli(getPosts...)
+	has := bytes.Contains(out, []byte(`"type": "post"`))
+	a.True(has, "subset returned a post as result")
+	a.EqualValues(0, srv.ReceiveLog.Seq(), "first message")
+
+	feedAlice, err := refs.NewFeedRefFromBytes(bytes.Repeat([]byte{1}, 32), refs.RefAlgoFeedSSB1)
+	r.NoError(err)
+
+	// publish a contact message for a new feed
+	_, _ = sbotcli("publish", "contact", "--following", feedAlice.String())
+	out, _ = sbotcli(getFollows...)
+	a.EqualValues(1, srv.ReceiveLog.Seq(), "2nd message")
+	has = bytes.Contains(out, []byte(`"type": "contact"`))
+	a.True(has, "subset returned follow message in log")
+	_, _ = sbotcli(getByAuthor...)
 
 	srv.Shutdown()
 	err = srv.Close()
