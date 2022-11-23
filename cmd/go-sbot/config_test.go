db2ddd6f42534f70289bfc3e79d301c0ab2d152c
cblgh <cblgh@cblgh.org>
2022-11-23 12:21:25 +0100

    expand repo path according to expectations

--- cmd/go-sbot/config_test.go
+++ cmd/go-sbot/config_test.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"fmt"
 	"time"
 
 	"github.com/stretchr/testify/require"
@@ -188,4 +189,22 @@ numRepl = 10
 	r.EqualValues(expectedConfig.NoUnixSocket, runningConfig.NoUnixSocket)
 	r.EqualValues(expectedConfig.NumPeer, runningConfig.NumPeer)
 	r.EqualValues(expectedConfig.NumRepl, runningConfig.NumRepl)
+}
+
+func TestConfigRepoPathExpands(t *testing.T) {
+	r := require.New(t)
+	repodir := ".test-ssb"
+	configContents := fmt.Sprintf(`repo = "%s"`, repodir)
+
+	testPath := filepath.Join(".", "testrun", t.Name())
+	r.NoError(os.RemoveAll(testPath), "remove testrun folder")
+	r.NoError(os.MkdirAll(testPath, 0700), "make new testrun folder")
+	configPath := filepath.Join(testPath, "config.toml")
+	err := os.WriteFile(configPath, []byte(configContents), 0700)
+	r.NoError(err, "write config file")
+	parsedConfig := readConfig(configPath)
+
+	home, err := os.UserHomeDir()
+	r.NoError(err, "get user home dir")
+	r.EqualValues(filepath.Join(home, repodir), parsedConfig.Repo, "repo dir should expand to be relative to home dir")
 }
